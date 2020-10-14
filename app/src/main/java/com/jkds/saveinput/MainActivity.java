27e1fa1a0aae6b17374576b6f7e522fab3ce16e7
wjkcdl <m15213862451@163.com>
2020-10-14 14:35:59 +0800

    初始化sp

--- app/src/main/java/com/jkds/saveinput/MainActivity.java
+++ app/src/main/java/com/jkds/saveinput/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SPUtils.getInstance(MainActivity.this);
         listMap = new ArrayList<String>();
         sLinerLayout = findViewById(R.id.ll);
         List<String> listMap = SPUtils.getDataListpz("listMap", MainActivity.this);
